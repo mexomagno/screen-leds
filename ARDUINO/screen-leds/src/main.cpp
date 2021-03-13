79bc6a680110c59c9db5e1078b013b338598fcaa
Maximiliano Castro <max@djavu.cl>
2021-03-13 20:27:31 -0300

    dont know

--- ARDUINO/screen-leds/src/main.cpp
+++ ARDUINO/screen-leds/src/main.cpp
@@ -1,12 +1,27 @@
 #include <Arduino.h>
-
-void setup() {
-  pinMode(13, OUTPUT);
+// La velocidad depende del modelo de ESP-01
+// siendo habituales 9600 y 115200
+const long baudRate = 9600;
+ 
+#include "SoftwareSerial.h"
+SoftwareSerial softSerial(2, 3); // RX, TX
+ 
+void setup()
+{
+   Serial.begin(baudRate);
+   softSerial.begin(baudRate);
+   delay(5000);
 }
+ 
+void loop(){
+   String in = "";
+   boolean ready = false;
 
-void loop() {
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(500);
+   while(softSerial.available()) {
+      in = softSerial.readString();
+      ready = true;
+   }
+   if (ready) {
+      Serial.println("Receiving string"  + in);
+   }
 }
